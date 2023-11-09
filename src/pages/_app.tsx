70817d7899a7012e4f349f2a8a1525b259803b2c
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-11-09 17:38:09 -0600

    Dleted prettier config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
 import { type AppType } from "next/app";
-import {
-  ClerkProvider,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-
   return (
     <ClerkProvider {...pageProps}>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: "dark" }}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: "dark" }}
+      >
+        <Component {...pageProps} />
       </MantineProvider>
     </ClerkProvider>
   );
